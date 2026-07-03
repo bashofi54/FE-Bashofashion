df94cff68faf22deda69b6d28e98f31e9e20588c
bashofi54 <bashofi54@gmail.com>
2026-07-03 09:31:19 +0700

    Fix: Downgrade toast ke v0.6.1 Vue2

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import Toast from "vue-toast-notification"; // <-- ini tetep
+import "vue-toast-notification/dist/theme-sugar.css"; // <-- css nya
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -9,6 +11,9 @@ import '@/assets/css/main.css'
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
+Vue.use(Toast, { // <-- daftarin di sini
+  position: 'top-right'
+});
 
 Vue.config.productionTip = false
 
